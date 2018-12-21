014e2a40362b5f1d1b642e517876eb0aab553281
maobui <mao.buidinh@hotmail.com>
2018-12-21 16:50:57 +0700

    - Implement.

--- app/src/main/java/com/me/bui/mintsplunktest/MainActivity.kt
+++ app/src/main/java/com/me/bui/mintsplunktest/MainActivity.kt
@@ -4,13 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import com.splunk.mint.Mint
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Mint.initAndStartSession(this.application, "977c7be6")
         btnCrash.setOnClickListener { view -> forceCrash(view) }
         btnException.setOnClickListener { _ -> forceException() }
     }
